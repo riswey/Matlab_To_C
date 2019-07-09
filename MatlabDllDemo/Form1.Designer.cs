6f4bd130c5ff1a130d6f2bf468964bfa410d9ab1
Alva Gosson <riswey@hotmail.com>
2019-07-09 11:35:28 +0100

    Working

--- MatlabDllDemo/Form1.Designer.cs
+++ MatlabDllDemo/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(71, 66);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(663, 159);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(71, 255);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(663, 166);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(626, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Demo using DLLs created purely by Matlab and imported into c#";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MatlabDllDemo/Form1.cs
+++ MatlabDllDemo/Form1.cs
@@ -4,17 +4,95 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace MatlabDllDemo
 {
     public partial class Form1 : Form
     {
+        [DllImport("GenSig.dll")]
+        public static extern void GenSig(double[] time_vals_data, int[] time_vals_size, double[] signal_data, int[] signal_size);
+        [DllImport("GenSig.dll")]
+        public static extern void GenSig_initialize();
+        [DllImport("GenSig.dll")]
+        public static extern void GenSig_terminate();
+
+        [DllImport("CalcSpec.dll")]
+        public static extern void CalcSpec(double[] signal_, double[] spectrum);
+        [DllImport("CalcSpec.dll")]
+        public static extern void CalcSpec_initialize();
+        [DllImport("CalcSpec.dll")]
+        public static extern void CalcSpec_terminate();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetSignal(out double[] signal_data);
+
+            chart1.Legends.Clear();
+            chart2.Legends.Clear();
+
+            chart1.Series["Series1"].ChartType = SeriesChartType.Line;
+            for (int i=0;i<2001;i++) {
+                chart1.Series["Series1"].Points.AddXY(i * 0.01, signal_data[i]);
+            }
+            //chart title  
+            chart1.Titles.Add("Signal");
+
+            GetSpectrum(signal_data, out double[] spectrum);
+            chart2.Series["Series1"].ChartType = SeriesChartType.Line;
+            for (int i = 0; i < 1001; i++)
+            {
+                chart2.Series["Series1"].Points.AddXY((double)i / 20, spectrum[i]);
+            }
+            //chart title  
+            chart2.Titles.Add("Spectrum");
+
+        }
+
+
+        static void GetSignal(out double[] signal_data)
+        {
+            //Declare and Initialse
+            int[] time_vals_size = new int[1] { 2001 };
+            double[] time_vals_data = new double[2001];
+            {
+                double counter = 0.0;
+                for (int i = 0; i < 2001; i++)
+                {
+                    time_vals_data[i] = counter;
+                    counter += 0.01;
+                }
+            }
+
+            //Result array
+            signal_data = new double[2001];
+
+            int[] signal_size = new int[2]; //what does this do?
+
+            //Only needs be called once
+            GenSig_initialize();
+            //Make function calls freely
+            GenSig(time_vals_data, time_vals_size, signal_data, signal_size);
+            //Only needs be called once
+            GenSig_terminate();
+        }
+
+        static void GetSpectrum(double[] signal_data, out double[] spectrum)
+        {
+            spectrum = new double[2001];
+            CalcSpec_initialize();
+            CalcSpec(signal_data, spectrum);
+            CalcSpec_terminate();
+        }
+
     }
 }
